97cf51beefa800b9696f3dd562434ce4a260e6ec
cloud5414 <cloud5414@gmail.com>
2024-05-20 07:47:11 +0900

    CheckStyle警告対応

--- src/test/java/wacky/horseeggs/LoreWriter/factory/LoreWriterFactoryTest.java
+++ src/test/java/wacky/horseeggs/LoreWriter/factory/LoreWriterFactoryTest.java
@@ -1,11 +1,12 @@
 /**
- * 
+ * Factory for lore writer.
  */
+
 package wacky.horseeggs.LoreWriter.factory;
 
-import static org.junit.Assert.*;
 import java.util.Objects;
 import org.bukkit.entity.EntityType;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -14,14 +15,16 @@ import wacky.horseeggs.LoreWriter.LoreWriter;
 import wacky.horseeggs.eggData.EggDataBase;
 
 /**
- * 
+ * {@link LoreWriterFactory} クラスを検証するテストクラスです.
  */
 public class LoreWriterFactoryTest {
   @Mock
   EggDataBase eggData;
 
   /**
-   * @throws java.lang.Exception
+   * 事前設定.
+   *
+   * @throws java.lang.Exception すべての例外
    */
   @Before
   public void setUp() throws Exception {
@@ -33,41 +36,42 @@ public class LoreWriterFactoryTest {
   }
 
   /**
-   * {@link wacky.horseeggs.LoreWriter.factory.LoreWriterFactory#newLoreWriter(org.bukkit.entity.EntityType, wacky.horseeggs.eggData.EggDataBase)} のためのテスト・メソッド。
+   * {@link LoreWriterFactory#newLoreWriter(EntityType, EggDataBase)}
+   * のためのテスト・メソッド.
    */
   @Test
   public final void testNewLoreWriter() {
     // ラマ
     LoreWriter llamaLw = LoreWriterFactory.newLoreWriter(EntityType.LLAMA, eggData);
-    assertTrue(Objects.nonNull(llamaLw));
+    Assert.assertTrue(Objects.nonNull(llamaLw));
 
     // ラバ
     LoreWriter muleLw = LoreWriterFactory.newLoreWriter(EntityType.MULE, eggData);
-    assertTrue(Objects.nonNull(muleLw));
-    
+    Assert.assertTrue(Objects.nonNull(muleLw));
+
     // ロバ
     LoreWriter donkeyLw = LoreWriterFactory.newLoreWriter(EntityType.DONKEY, eggData);
-    assertTrue(Objects.nonNull(donkeyLw));
-    
+    Assert.assertTrue(Objects.nonNull(donkeyLw));
+
     // ウマ
     LoreWriter horseLw = LoreWriterFactory.newLoreWriter(EntityType.HORSE, eggData);
-    assertTrue(Objects.nonNull(horseLw));
-    
+    Assert.assertTrue(Objects.nonNull(horseLw));
+
     // ゾンビホース
     LoreWriter zombieHorseLw = LoreWriterFactory.newLoreWriter(EntityType.ZOMBIE_HORSE, eggData);
-    assertTrue(Objects.nonNull(zombieHorseLw));
-    
+    Assert.assertTrue(Objects.nonNull(zombieHorseLw));
+
     // スケルトンホース
     LoreWriter skeltonHorseLw = LoreWriterFactory.newLoreWriter(EntityType.SKELETON_HORSE, eggData);
-    assertTrue(Objects.nonNull(skeltonHorseLw));
-    
+    Assert.assertTrue(Objects.nonNull(skeltonHorseLw));
+
     // 行商人のラマ
     LoreWriter traderLammaLw = LoreWriterFactory.newLoreWriter(EntityType.TRADER_LLAMA, eggData);
-    assertTrue(Objects.nonNull(traderLammaLw));
-    
+    Assert.assertTrue(Objects.nonNull(traderLammaLw));
+
     // コウモリだけが知っている
     LoreWriter otherLw = LoreWriterFactory.newLoreWriter(EntityType.BAT, eggData);
-    assertTrue(Objects.nonNull(otherLw));
+    Assert.assertTrue(Objects.nonNull(otherLw));
   }
 
 }
